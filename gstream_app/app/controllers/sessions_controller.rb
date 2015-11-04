882848f1872791badc0b84d4672baff5659c9ec7
Oleg <oleg.kord@gmail.com>
2015-11-04 17:45:35 -0500

    added session infrastructure

---
+++ gstream_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(user_name: params[:user_name])
+    if user && user.authenticalte(params[:password])
+      session[:user_id] = user.id
+      redirect_to root
+    else
+#---->CHANGE TO ERROR FOR PASSWORD!!
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to "/login"
+  end
+
+end
