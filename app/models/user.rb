8b096f2be8b0a610ac38d5cefdb172571b90e53d
Oleg <oleg.kord@gmail.com>
2015-11-05 16:57:51 -0500

    fixed login bug and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :genres
-  
+
   validates :user_name, presence: true
   validates :user_name, uniqueness: true
 
-  validates :user_email, presence: true
-  validates :user_email, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
