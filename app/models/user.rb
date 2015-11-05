d5774a9f4c867e2c92cd235dbcc35a2b62ef607c
Oleg <oleg.kord@gmail.com>
2015-11-05 18:55:58 -0500

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true
   validates :email, uniqueness: true
+
+   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
