07c7d08dfebb7c5f309f4cbf1a6583808a891099
Oleg <oleg.kord@gmail.com>
2015-11-06 15:37:00 -0500

    Artists#new working

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,7 @@ class ArtistsController < ApplicationController
 
   def new
     @artist = Artist.new
+    @genre = Genre.find(params["genre_id"])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,6 @@ class UsersController < ApplicationController
     end
   end
 
-
-
   private
   def user_params
     params.require(:user).permit(
