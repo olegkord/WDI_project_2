20a6376077c851f18be2653d7340f9312fcdc5d5
Oleg <oleg.kord@gmail.com>
2015-11-07 16:56:37 -0500

    created song#new

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,9 @@
 class SongsController < ApplicationController
 
 def new
-
+  @song = Song.new
+  @artist = Artist.find(params["artist_id"])
+  @genre = Genre.find(params["genre_id"])
 end
 
 def create
